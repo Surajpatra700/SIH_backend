06a2cfec8e006145bdec7792a5ffb42435c56a3c
Suraj Patra <surajpatra2908@gmail.com>
2023-09-22 20:19:02 +0530

    added index.html

--- index.js
+++ index.js
@@ -2,24 +2,34 @@ require('dotenv').config();
 const express = require('express');
 const port = process.env.PORT || 6200;
 const bodyparser = require('body-parser');
+const path = require('path');
 
 const app = express();
 // app.use(express.json);
 app.use(bodyparser.json());
-app.use(bodyparser.urlencoded({extended: true}));
+app.use(bodyparser.urlencoded({ extended: true }));
 
-app.listen(port, (error)=> {
-    if(!error)
+app.listen(port, (error) => {
+    if (!error)
         console.log("Server is running at port " + port);
     else
-        console.log("Error Occured ",error);
+        console.log("Error Occured ", error);
 });
 
-app.get("/", (req, res)=>{
+app.get("/", (req, res) => {
     res.send("Home Screen");
 });
 
-app.post("/", (req,res)=>{
-    const {name} = req.body;
+// app.get('/html', (req, res) => {
+//     res.sendFile(__dirname + './index.html')
+// });
+
+app.get('/html', (req, res) => {
+    const filePath = path.join(__dirname, 'index.html');
+    res.sendFile(filePath);
+});
+
+app.post("/", (req, res) => {
+    const { name } = req.body;
     res.send('Welcome ' + name);
 });
